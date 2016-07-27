8d99171fdec91300e3574a87e4e59365f1ecd4b0
ahgsa-unl <nebraska.ahgsa@gmail.com>
2016-07-27 16:19:11 -0500

    Add files via upload
    Script from first meeting (2016-07-26)

---
+++ Meeting#1.R
@@ -0,0 +1,71 @@
+######################
+# R Club meeting one #
+######################
+
+# 3) R terminology
+# Object
+a<- c(1,4,7,9)
+a
+
+# Functions
+help()
+help("mean")
+mean(a)
+
+# Arguments within functions
+mean(x=a)
+
+# Package installation
+install.packages("ggplot2")
+
+# Loading packages
+library(ggplot2)
+
+# 4) Importing a dataset
+# Setting working directory
+setwd("/Users/lmbastos/Dropbox/1 - UNL/AHGSA/R Club/Meeting #01/data")
+
+# Reading cotton dataset and saving it to object called "cotton"
+cotton <- read.csv("cotton.csv") 
+
+# Variable types
+summary(cotton)
+str(cotton)
+class(cotton$lineage)
+
+# Resampling a dataset
+# First row only
+cotton[1,]
+
+# First column only
+cotton[,1]
+
+# Rows 1 through 4 and columns 1 through 3
+cotton[1:4,1:3]
+
+# Rows 1 and 4 and columns 1 and 5
+cotton[c(1,4),c(1,5)]
+
+# 5) Exploratory statistical analysis
+# Histogram
+ggplot(data=cotton, aes(x=yield))+
+  geom_histogram()
+
+# Density plot
+ggplot(data=cotton, aes(x=yield))+
+  geom_histogram()
+
+# Boxplot
+ggplot(data=cotton, aes(x=block, y=yield))+
+  geom_boxplot()
+
+# Violin plot
+ggplot(data=cotton, aes(x=block, y=yield))+
+  geom_violin()
+
+
+
+
+
+
+
